f25392ccad1b8d983350f12c1ca521a1647a3e6d
MIHAIL <MIK.MARSSS@MAIL.RU>
2024-09-09 23:55:48 +0700

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.jsx'
 import { RecoilRoot } from 'recoil';
-
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RecoilRoot>
-      <App />
+      <BrowserRouter basename="/CubeTheGame">
+        <App />
+      </BrowserRouter>
     </RecoilRoot>
   </StrictMode>,
 )
